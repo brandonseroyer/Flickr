27468fc7b344e7dcd76a1c18aa7256dee300d6ea
Brandon Seroyer & Tyler Finley <student@epicodus.com>
2015-10-22 12:45:00 -0700

    set standard aspec rations for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,6 @@ class Post < ActiveRecord::Base
   has_and_belongs_to_many :tags
   has_many :comments, dependent: :destroy
 
-  has_attached_file :image, :styles => { :small => "150x150", :medium => "300x300>", :large => "750x750", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => {:standard => "750x500", :profile => "1250x830", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
